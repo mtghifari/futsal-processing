93579b3ee143d60279ccfbdfb514fcee5164a9e7
mtghifari <thafhanghifari@gmail.com>
2025-10-08 15:07:25 +0700

    Update and rename streamlit_app.py to 0_Home.py

---
+++ 0_Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🎈 Selamat datang di Pengolahan Data Futsal")
+st.write(
+    "Seluruh tools pada dashboard ini dibuat untuk memudahkan pengolahan advance analysis futsal."
+)
